6e35389b7456f83a4bcb6cca962f51338d0515df
bangwoojord <miksik2001@gmail.com>
2021-03-19 23:17:52 +0200

    Added a func to extract file name from its full path

--- headers/file_related.h
+++ headers/file_related.h
@@ -15,5 +15,7 @@ void process_data(long double data);
 //calculates amount of characters in a binary file
 uint32_t file_length(std::string const &path);
 
+//removes path from full file path - aka extracts file name
+bool remove_path(std::string &full_path);
 
 #endif //QT_FILE_PROCESSING_FILE_RELATED_H
--- src/file_related.cpp
+++ src/file_related.cpp
@@ -1,4 +1,5 @@
 #include "../headers/file_related.h"
+#include <cstring>
 
 int binary_write(std::string const &filename, std::string const &path, const char *data){
     char dash = (path.back() != '\\' && path.back() != '/')? '/' : NULL; // checking if the file path came with / \ on the end !=> assigning one
@@ -39,4 +40,14 @@ uint32_t file_length(std::string const &path){
     uint32_t file_size = file_read.tellg();
     file_read.close();
     return file_size;
+}
+
+bool remove_path(std::string &full_path){
+    for(auto i = full_path.size()-1; i >=0; --i){
+        if(full_path[i] == '\\' || full_path[i] == '/'){
+            full_path.erase(0, i+1);
+            return true;
+        }
+    }
+    return false;
 }
