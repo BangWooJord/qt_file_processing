c0412e98095359faa411d5a10ba2f1dbfb9dd0a9
BangWooJord <58793833+BangWooJord@users.noreply.github.com>
2021-03-20 19:03:01 +0200

    Fixed the problem with incorrect reading, as i thought it was caused by incorrect path :)

---
+++ headers/toml_related.h
@@ -0,0 +1,10 @@
+#ifndef QT_FILE_PROCESSING_TOML_RELATED_H
+#define QT_FILE_PROCESSING_TOML_RELATED_H
+#include <iostream>
+#include <set>
+#include <vector>
+#include <toml11-3.6.0/toml.hpp>
+
+bool openConfig(std::string const &cfg_path, std::string const &value, std::set<std::string> &unique_path);
+
+#endif //QT_FILE_PROCESSING_TOML_RELATED_H
--- main.cpp
+++ main.cpp
@@ -1,27 +1,11 @@
 #include <QApplication>
 #include "qt_interface/interfacewidget.h"
+#include "headers/toml_related.h"
 #include <iostream>
 #include <thread>
 #include <vector>
 #include <set>
-#include <toml11-3.6.0/toml.hpp>
 
-bool openConfig(std::string const &cfg_path, std::string const &value, std::set<std::string> &unique_path){
-    auto cfg_data = toml::parse(cfg_path);
-    std::vector<std::string> data;
-    try {
-        data = toml::find<std::vector<std::string>>(cfg_data, value);
-    }catch(const std::out_of_range& e){
-        std::cerr << "Couldn't find specified string vector" << std::endl;
-        return false;
-    }
-    for(const auto &file: data){ //rewrite all file names into a set, if file name matches with the name inside of set - don't save it
-        if(unique_path.find(file) == unique_path.end()) {
-            unique_path.insert(file);
-        }
-    }
-    return true;
-}
 //TODO: юнит тесты
 
 int main(int argc, char *argv[]) {
