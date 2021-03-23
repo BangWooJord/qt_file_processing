7f1a780fb991238683cfc18de2b5a30579a6775f
BangWooJord <58793833+BangWooJord@users.noreply.github.com>
2021-03-23 15:17:06 +0200

    Added test case into unit-test file

--- Tests/main-test.cpp
+++ Tests/main-test.cpp
@@ -1,16 +1,33 @@
 #define CATCH_CONFIG_MAIN
 #include <Catch2-2.13.4/single_include/catch2/catch.hpp>
 #include "file_related.h"
+#include <thread>
 #include "toml_related.h"
 
 TEST_CASE("Toml reading function working"){
     std::set<std::string> path;
+
     REQUIRE(openConfig("../../toml_cfg/cfg.toml", "paths", path) == true);
 }
 
-TEST_CASE("File length reading function working"){
-    REQUIRE(file_length("../../example_files/file1.bin") == 480622657);
-    REQUIRE(file_length("../../example_files/file2.bin") == 800000497);
-    REQUIRE(file_length("../../example_files/file3.bin") == 400000000);
-    REQUIRE(file_length("../../example_files/file4.bin") == 404400004);
+TEST_CASE("File reading OK"){
+    long double data;
+
+    std::thread test1([&](){
+        REQUIRE(binary_read("../../example_files/file1.bin", data) == 0);
+    });
+    std::thread test2([&](){
+        REQUIRE(binary_read("../../example_files/file2.bin", data) == 0);
+    });
+    std::thread test3([&](){
+        REQUIRE(binary_read("../../example_files/file3.bin", data) == 0);
+    });
+    std::thread test4([&](){
+        REQUIRE(binary_read("../../example_files/file4.bin", data) == 0);
+    });
+    REQUIRE(binary_read("random_text", data) < 0);
+    test1.join();
+    test2.join();
+    test3.join();
+    test4.join();
 }
